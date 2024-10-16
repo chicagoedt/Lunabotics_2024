266e6aab84f51adfbaaa86ed67cb40f62a290050
Mull <85046208+educationmoment@users.noreply.github.com>
2024-10-16 14:59:24 -0500

    Delete webgui directory

--- webgui/setmode.js
+++
@@ -1,9 +0,0 @@
-function setMode(mode) {
-    fetch(`/set_mode/${mode}`, { method: 'POST' })
-        .then(response => response.json())
-        .then(data => {
-            alert(`Robot set to ${data.mode} mode`);
-            toggleMenu();
-        })
-        .catch(error => console.error('Error:', error));
-}
