35ddf3e28c93bf0d0dfe821e9d3b0f47cc5de798
Mull <85046208+educationmoment@users.noreply.github.com>
2024-10-16 19:16:59 -0500

    Update server.py

--- webgui/server.py
+++ webgui/server.py
@@ -1,46 +1,52 @@
-from flask import Flask, Response, render_template, session
+from flask import Flask, Response, render_template, request
 import cv2
 import subprocess
 import time
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, disconnect
 import pty
 import os
 import select
-# make sure to install flask-socketio on jetson b4 running, otherwise itll poop :(
+
 app = Flask(__name__)
-app.secret_key = 'colton_is_a_latina_baddie'
+app.secret_key = 'secret!'
 socketio = SocketIO(app)
-# unrelated but if we wanted 2 be more secure: we could implement a shell sandbox, / display over https / make a better pw than coltin is a latnia baddie
+
+# stupid dictinary fix pls
+fd_dict = {}
+
 def generate_frames():
-    
+
     cap = cv2.VideoCapture(0)
 
     while True:
-        
+
         success, frame = cap.read()
         if not success:
             break
         
-        
+
         ret, buffer = cv2.imencode('.jpg', frame)
         if not ret:
             continue
         
         frame_bytes = buffer.tobytes()
         
-       
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
     cap.release()
 
-def read_and_forward_pty_output(fd):
+def read_and_forward_pty_output(sid, fd):
     max_read_bytes = 1024 * 20
     while True:
         socketio.sleep(0.01)
-        if fd in select.select([fd], [], [], 0)[0]:
-            output = os.read(fd, max_read_bytes).decode()
-            socketio.emit('pty_output', {'output': output}, namespace='/pty')
+        try:
+            if fd in select.select([fd], [], [], 0)[0]:
+                output = os.read(fd, max_read_bytes).decode()
+                socketio.emit('pty_output', {'output': output}, namespace='/pty', to=sid)
+        except OSError:
+            # file descriptor might have been closed
+            break
 
 @app.route('/')
 def index():
@@ -51,13 +57,9 @@ def video_feed():
     return Response(generate_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/terminal')
-def terminal():
-    return render_template('terminal.html')
-
 @socketio.on('connect', namespace='/pty')
 def connect():
-    print('Client connected')
+    print('Client connected:', request.sid)
 
     # bash shell
     (child_pid, fd) = pty.fork()
@@ -66,18 +68,28 @@ def connect():
         os.execvp('bash', ['bash'])
     else:
         # parent process
-        socketio.start_background_task(target=read_and_forward_pty_output, fd=fd)
-        session['fd'] = fd
+        fd_dict[request.sid] = fd
+        socketio.start_background_task(target=read_and_forward_pty_output, sid=request.sid, fd=fd)
 
 @socketio.on('pty_input', namespace='/pty')
 def pty_input(data):
-    fd = session.get('fd')
+    fd = fd_dict.get(request.sid)
     if fd:
-        os.write(fd, data['input'].encode())
+        try:
+            os.write(fd, data['input'].encode())
+        except OSError:
+            # handle the error if fd is closed
+            pass
+    else:
+        print('No fd found for session:', request.sid)
 
 @socketio.on('disconnect', namespace='/pty')
 def disconnect():
-    print('Client disconnected')
+    print('Client disconnected:', request.sid)
+    fd = fd_dict.get(request.sid)
+    if fd:
+        os.close(fd)
+        del fd_dict[request.sid]
 
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port=5000, debug=True)
+    socketio.run(app, host='192.168.0.0', port=5000, debug=True)
