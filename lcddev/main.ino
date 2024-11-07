61b5dc2bc3b650b250a6264ffc605f65278348f7
Mull <85046208+educationmoment@users.noreply.github.com>
2024-11-06 19:06:30 -0600

    Update main.ino

--- lcddev/main.ino
+++ lcddev/main.ino
@@ -1,18 +1,17 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2); // Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27,16,2); 
 
 void setup() {
-  lcd.init(); // Initialize the LCD
-  lcd.backlight(); // Turn on the backlight
+  lcd.init(); 
+  lcd.backlight(); 
 
-  Serial.begin(9600); // Initialize serial communication at 9600 bps
+  Serial.begin(9600); 
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    // Read the incoming strings until newline characters
     String line1 = Serial.readStringUntil('\n');
     String line2 = Serial.readStringUntil('\n');
 
