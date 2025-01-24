0457702fecda8fdbf20aeadf0c5db96b886d0b44
Aakash B <85046208+educationmoment@users.noreply.github.com>
2025-01-23 19:37:32 -0600

    Update lunabotics_launch.py

--- launch/lunabotics_launch.py
+++ launch/lunabotics_launch.py
@@ -4,7 +4,8 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(package='control_pkg', executable='testcontrol.py', output='screen'),
-        Node(package='control_pkg', executable='drivetrain', output='screen'),
-        Node(package='control_pkg', executable='scoop', output='screen')
-        # etc.
+        Node(package='motor_control_pkg', executable='drivetrain', output='screen'),
+        Node(package='motor_control_pkg', executable='scoop', output='screen'),
+        Node(package='brain_pkg', executable='brain.py', output='screen'),
+        Node(package='control_pkg', executable='canable_start.sh', output='screen')
     ])
